8216adb902994478bc5ba8b08041c2c757475b4e
PR00362687 <PR00362687@UKMKCSLP03731.TechMahindra.com>
2017-08-14 15:56:44 +0200

    code changes

---
+++ src/com/techm/test.java
@@ -0,0 +1,23 @@
+package com.techm;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class test {
+
+	public static void main(String[] args) throws InterruptedException, MalformedURLException {
+		// TODO Auto-generated method stub
+		//C:\Program Files (x86)\Google\Chrome\Application
+		
+		
+		System.setProperty("webdriver.chrome.driver", "Mozilla Firefox/firefox.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		System.out.println("driver opened");
+		driver.quit();
+		
+	}
+
+}
